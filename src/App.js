fa17a63f6d20ff4a351caccaa7a39d1b5df27a4c
BogDone <buselb2@gmail.com>
2021-05-07 16:40:56 +0300

    add her section

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Home  from './components/pages/Home';
 
 function App() {
   return (
     <>
       <Router>
-      <Navbar />
-      <Switch>
-        <Route path='/' exact />
-      </Switch>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+        </Switch>
       </Router>
     </>
   );
