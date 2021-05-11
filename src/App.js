c00bd0f0a77e774e9060fb625334151e4969a843
BogDone <buselb2@gmail.com>
2021-05-11 11:44:12 +0300

    final upd

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Home  from './components/pages/Home';
 import Services  from './components/pages/Services';
 import Products  from './components/pages/Products';
 import SignUp  from './components/pages/SignUp';
-
 function App() {
   return (
     <>
